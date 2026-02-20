69a6fa6a023803eb3918bc7a7198dcfce36702a2
Darshmenon08 <darsh@asimovrobotics.com>
2026-02-20 16:49:55 +0530

    Add missing entry points to CMakeLists and setup.py

--- src/pickplace_rl_mobile/setup.py
+++ src/pickplace_rl_mobile/setup.py
@@ -4,28 +4,32 @@ package_name = 'pickplace_rl_mobile'
 
 setup(
     name=package_name,
-    version='0.0.1',
+    version='0.1.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', [
-            'launch/display_launch.py', 
+            'launch/display_launch.py',
             'launch/gazebo_launch.py',
             'launch/gazebo.launch.py',
             'launch/rl_train.launch.py',
-            'launch/standalone_rl_training.launch.py'
+            'launch/standalone_rl_training.launch.py',
+            'launch/full_system.launch.py',
         ]),
         ('share/' + package_name + '/urdf', ['urdf/pickplace_mobile_arm.urdf']),
         ('share/' + package_name + '/worlds', ['worlds/pickplace_world.world']),
-        ('share/' + package_name + '/config', ['config/training_config.yaml']),
+        ('share/' + package_name + '/config', [
+            'config/training_config.yaml',
+            'config/nav2_params.yaml',
+        ]),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Your Name',
-    maintainer_email='your_email@example.com',
-    description='Pick and place RL mobile robot package',
+    maintainer='Darsh Menon',
+    maintainer_email='darshmenon02@gmail.com',
+    description='Pick-and-place RL mobile manipulator with perception, Nav2, and safety',   
     license='MIT',
     tests_require=['pytest'],
     entry_points={
