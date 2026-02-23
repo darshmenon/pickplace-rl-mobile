1f0efa10029ba922f85fa4122ecc09ae014b6636
Darshmenon08 <darsh@asimovrobotics.com>
2026-02-23 12:24:54 +0530

    feat: Add mobile ur3 combined URDF and launch file

---
+++ src/pickplace_rl_mobile/launch/display_mobile_ur3.launch.py
@@ -0,0 +1,34 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    pkg_path = get_package_share_directory('pickplace_rl_mobile')
+    urdf_file = os.path.join(pkg_path, 'urdf', 'mobile_ur3.urdf')
+    rviz_config = os.path.join(pkg_path, 'rviz', 'urdf.rviz')
+
+    with open(urdf_file, 'r') as infp:
+        robot_desc = infp.read()
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': robot_desc}]
+        ),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui'
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            # arguments=['-d', rviz_config] # Skipping config for now to let it load default
+        )
+    ])
